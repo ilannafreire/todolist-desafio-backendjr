f45621c0e40d3f10c7e7aa5113f259e2bf80ec12
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-11 04:45:32 -0300

    feat: contrução da service

---
+++ src/main/java/br/com/ilannafreire/service/TodoService.java
@@ -0,0 +1,60 @@
+package br.com.ilannafreire.service;
+
+
+import br.com.ilannafreire.entity.Todo;
+import br.com.ilannafreire.repository.TodoRepository;
+import io.micrometer.observation.Observation;
+import org.apache.coyote.BadRequestException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "prioridade")
+                .and(Sort.by(Sort.Direction.ASC, "id"));
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> update(Long id, Todo todo) {
+        todoRepository.findById(id).ifPresentOrElse((existingTodo) -> {
+            todo.setId(id);
+            todoRepository.save(todo);
+        }, () -> {
+            try {
+                throw new BadRequestException("Todo %d não existe! ".formatted(id));
+            } catch (BadRequestException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return list();
+
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.findById(id).ifPresentOrElse((existingTodo) -> todoRepository.delete(existingTodo), () -> {
+            try {
+                throw new BadRequestException("Todo %d não existe! ".formatted(id));
+            } catch (BadRequestException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return list();
+    }
+}
