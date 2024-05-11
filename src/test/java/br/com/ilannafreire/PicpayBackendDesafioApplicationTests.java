b6a72f2fb0dc0bd84077831dfa9f2e67f45b17c1
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-11 00:46:19 -0300

    initial commit

---
+++ src/test/java/br/com/ilannafreire/PicpayBackendDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ilannafreire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
